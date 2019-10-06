ea233d5c6fa9a4f729a17a7f52f2b216835836a3
hzyhzya <55303885+hzyhzya@users.noreply.github.com>
2019-10-05 18:35:25 -0700

    Update faster-points.js

--- faster-points.js
+++ faster-points.js
@@ -10,9 +10,11 @@ function draw() {
   //stroke(x, y, 100);
   //point(x, y);
   {
-    for (var x = random(width); x <= width; x = x + 10) {
-    for (var y = random(height); y <= height; y = y + 10) {
-    stroke(x, y, 100)
-    point(x, y)}}
+    for (var i = 0; i<100; i++){
+      var x = random(width);
+      var y = random(height)
+      
+      stroke(x, y, 100);
+      point(x, y);
 }
-}
+}}
